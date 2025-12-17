6d456fa19651ab9df5868b85947973316deac782
nealb03 <nealb03@email.franklin.edu>
2025-12-16 21:25:01 -0500

    Update vite.config.js

--- Frontend01/vite.config.js
+++ Frontend01/vite.config.js
@@ -1,16 +1,14 @@
-// vite.config.js
+// Frontend01/vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // You can customize as needed:
-  // root: './',          // if your index.html is in a different folder
-  // build: {
-  //   outDir: 'dist',
-  // },
   server: {
-    port: 5173,
+    port: 5173
   },
+  build: {
+    outDir: 'dist'
+  }
 })
