6e23a3d095b782c80f133be3b9ec42f0bacfbf9a
Brent Neal <bneal5@twc.com>
2026-01-08 19:49:30 -0500

    Fix app1 outputs to match EC2/RDS/S3 resources

--- terraform/app1-s3-ec2-rds/outputs.tf
+++ terraform/app1-s3-ec2-rds/outputs.tf
@@ -8,7 +8,17 @@ output "rds_endpoint" {
   value       = aws_db_instance.cloud495.endpoint
 }
 
+output "rds_address" {
+  description = "RDS hostname/address."
+  value       = aws_db_instance.cloud495.address
+}
+
 output "s3_website_endpoint" {
-  description = "S3 website endpoint (if enable_s3_website = true)."
+  description = "S3 static website endpoint (if enable_s3_website = true)."
   value       = var.enable_s3_website ? aws_s3_bucket_website_configuration.frontend_website[0].website_endpoint : null
+}
+
+output "s3_bucket_name" {
+  description = "Frontend S3 bucket name (if enable_s3_website = true)."
+  value       = var.enable_s3_website ? aws_s3_bucket.frontend_bucket[0].bucket : null
 }
